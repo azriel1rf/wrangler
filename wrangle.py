a56083219b62fd910551f6132a16af107dc34c42
azriel1rf <azriel.1rf@gmail.com>
2019-06-15 12:16:08 +0900

    pipeline with VotingClassifier

--- wrangle.py
+++ wrangle.py
@@ -2,8 +2,9 @@
 import pandas as pd
 
 #%%
-data = pd.read_csv("./input_data/bank/train.csv", sep=",", header=0, quotechar="\"")
-
+# data = pd.read_csv("../input_data/bank/train.csv", sep=",", header=0, quotechar="\"")
+test = pd.read_csv('./input_data/bank/test.csv', sep=",", header=0, quotechar="\"")
+#%%
 #%%
 #%%
 data
@@ -15,7 +16,8 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.svm import SVC
-
+from lightgbm.sklearn import LGBMClassifier
+from sklearn.ensemble import VotingClassifier
 
 #%%
 numeric_features = ['age', 'balance', 'duration', 'campaign', 'pdays', 'previous']
@@ -36,26 +38,80 @@ preprocessor = ColumnTransformer(
         ('cat', categorical_transformer, categorical_features)])
 
 #%%
-clf = Pipeline(steps=[('preprocessor', preprocessor),
-    ('classifier', SVC())])
+clf = Pipeline([
+    ('preprocessor', preprocessor),
+    ('classifier', VotingClassifier(estimators=[
+        ('svc', SVC()),
+        ('lgbm', LGBMClassifier()),
+    ]))
+])
 
 #%%
-classifier = 
+clf.fit(X, y)
 
+#%%
 #%%
 y = data.y
 X = data.drop(columns={'id', 'y'})
 
 #%%
-clf.fit(X, y)
-
+prepedX = preprocessor.fit_transform(X)
 
 #%%
-clf.predict(X)
-
+prepedX
 #%%
 
 #%%
+#%%
+clf_svc = SVC(probability=True)
 
 #%%
+clf_lgbm = LGBMClassifier()
+
+
+#%%
+clf_svc.fit(prepedX, y)
+
+#%%
+clf_lgbm.fit(prepedX, y)
+
+#%%
+y_svc = clf_svc.predict_proba(prepedX)
+
+#%%
+y_lgbm = clf_lgbm.predict_proba(prepedX)
+
+#%%
+eclf = VotingClassifier(estimators=[
+    ('svc', clf_svc),
+    ('lgbm', clf_lgbm),
+], voting='soft')
+
+#%%
+eclf.fit(prepedX, y)
+
+#%%
+y_est = eclf.predict(prepedX)
+
+#%%
+test_X = test.drop(columns={'id'})
+
+#%%
+preped_test_X = preprocessor.transform(test_X)
+
+#%%
+y_est = eclf.predict(preped_test_X)
+
+#%%
+submit = test[["id"]].copy()
+submit["pred"] = y_est
+
+#%%
+submit.to_csv(
+    path_or_buf="./submit/submit_voting_20190615.csv", # 出力先
+    sep=",",                                            # 区切り文字
+    index=False,                                        # indexの出力有無
+    header=False                                        # headerの出力有無
+)
+
 #%%
